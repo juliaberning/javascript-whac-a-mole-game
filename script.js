34f1d894d65ddbb9469eeeb1f2db09645361a07b
Julia <julia.berning@thenotcompany.com>
2024-12-23 22:58:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+let currentMoleTile;
+let currentPlantTile
+let score = 0;
+let gameOver = false;
+
+window.onload = function() {
+    setGame();
+};   
+
+function setGame() {
+    // Set up the grid for the game bord in html
+    for (let i = 0; i < 9; i++) {
+        // Create a div element for each cell
+        let tile = document.createElement("div");
+        // Add id's id="0" to "8"
+        tile.id =i.toString();
+        tile.addEventListener("click", selectTile);
+        // Take the tags and add them to the div element
+        document.getElementById("board").appendChild(tile);
+    }
+    setInterval(setMole, 1000);
+    setInterval(setPlant, 2000);
+}
+
+function getRandomTile() {
+    // Get a random number between 0 and 8
+    let number = Math.floor(Math.random() * 9);
+    return number.toString();
+}
+
+function setMole() {
+    if (gameOver) {
+        return;
+    }
+
+    if (currentMoleTile) {
+        currentMoleTile.innerHTML = "";
+    }
+
+    let mole = document.createElement("img");
+    mole.src = "./assets/images/monty-mole.png";
+
+    // Get a random tile (div tag) and add the mole image tag to it
+    let number = getRandomTile();
+    if (currentPlantTile && number === currentPlantTile.id) {
+        return;
+    }
+    currentMoleTile = document.getElementById(number);
+    currentMoleTile.appendChild(mole);
+}
+
+function setPlant() {
+    if (gameOver) {
+        return;
+    }
+    if (currentPlantTile) {
+        currentPlantTile.innerHTML = "";
+    }
+
+    let plant = document.createElement("img");
+    plant.src = "./assets/images/piranha-plant.png";
+
+    // Get a random tile (div tag) and add the plant image tag to it
+    let number = getRandomTile();
+    if (currentMoleTile && number === currentMoleTile.id) {
+        return;
+    }
+    currentPlantTile = document.getElementById(number);
+    currentPlantTile.appendChild(plant);
+}
+
+function selectTile() {
+    if (gameOver) {
+        return;
+    }
+    console.log(this.id);
+    if (this == currentMoleTile) {
+        score += 10;
+        document.getElementById("score").innerText = score.toString();
+    } else if (this == currentPlantTile) {
+        document.getElementById("score").innerText = "GAME OVER: " + score.toString();
+        gameOver = true;
+    }
+}
